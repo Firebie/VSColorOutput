8f2ab2ea91f3558f75d4f2961bb14d1a22945851
firebie <den.subs@gmail.com>
2014-11-17 09:53:23 +0200

    Update with recent visual studio versions

--- VSColorOutput/VsColorOutputPackage.cs
+++ VSColorOutput/VsColorOutputPackage.cs
@@ -10,7 +10,7 @@ namespace BlueOnionSoftware
     [ProvideOptionPage(typeof(VsColorOutputOptions), VsColorOutputOptions.Category, VsColorOutputOptions.SubCategory, 1000, 1001, true)]
     [ProvideProfile(typeof(VsColorOutputOptions), VsColorOutputOptions.Category, VsColorOutputOptions.SubCategory, 1000, 1001, true)]
     [ProvideAutoLoad("adfc4e64-0397-11d1-9f4e-00a0c911004f")] //UICONTEXT_NoSolution; always loads at startup
-    [InstalledProductRegistration("VSColorOutput", "Color output for build and debug windows - http://blueonionsoftware.com/vscoloroutput.aspx", "1.4.6")]
+    [InstalledProductRegistration("VSColorOutput", "Color output for build and debug windows - http://blueonionsoftware.com/vscoloroutput.aspx", "1.4.8.1")]
     public class VsColorOutputPackage : Package
     {
     }
